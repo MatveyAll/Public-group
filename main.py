8f71048d086890bcc5ede5f182351e732491addb
MatveyAll <Matvey0300@gmail.com>
2024-03-29 17:49:26 +0300

    moderation page

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, url_for, redirect, render_template, request, session
 from flask_sqlalchemy import SQLAlchemy
 import pandas as pd
 import sqlalchemy
 
 # подключение к базе
-engine = sqlalchemy.create_engine('sqlite:///flask-database')
+engine = sqlalchemy.create_engine('sqlite:///flask-database.db')
 app = Flask(__name__)
+app.secret_key = 'your_secret_key'
 
 
 class User:
@@ -15,24 +16,71 @@ class User:
         self.description = description
 
 
+class Post:
+    def __init__(self, title, type, user_name, text):
+        self.title = title
+        self.type = type
+        self.user_name = user_name
+        self.text = text
+
+
+@app.route('/')
+@app.route('/main')
+def main():
+    return 'Приветствую вас на нашем сайте!;)'
+
+
+@app.route('/moderation', methods=['GET', 'POST'])
+def moderation():
+    if session['username'] != 'admin' and session['password'] != '54321':
+        return 'Вы не являетесь админом и не можете модерировать записи'
+    # Получаем все записи, требующие модерации, из таблицы posts_proverka
+    df = pd.read_sql_table('posts_proverka', engine)
+    posts = df.to_dict('records')
+
+    if request.method == 'POST':
+        # Проверяем, было ли отправлено одобрение какой-либо записи
+        if 'approve_post' in request.form:
+            title = request.form['title']
+            user_name = request.form['user_name']
+            typed = request.form['type']
+            text = request.form['text']
+            post_id = request.form['post_id']
+
+            # Перемещаем запись из таблицы posts_proverka в posts
+            with engine.connect() as connection:
+                connection.execute(
+                    sqlalchemy.text(
+                        "INSERT INTO posts (post_id, title, user_name, type, text) VALUES (:post_id, :title, :user_name, :type, :text)"),
+                    {"post_id": post_id, "title": title, "user_name": user_name, "type": typed, "text": text}
+                )
+                # Удаляем запись из таблицы posts_proverka
+                connection.execute(
+                    sqlalchemy.text("DELETE FROM posts_proverka WHERE post_id = :post_id"),
+                    {"post_id": post_id}
+                )
+                connection.commit()
+
+    return render_template('moderation.html', posts=posts)
+
+
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
         new_user = User(request.form['username'], request.form['password'], request.form['description'])
 
-        # Чтение данных из таблицы "users" в DataFrame с помощью Pandas
         df = pd.read_sql_table('users', engine)
 
-        # Проверка наличия логина в столбце 'login'
         if new_user.login in df['login'].values:
             return 'Пользователь с таким логином уже существует!'
-        # добавление пользователя в базу
         with engine.connect() as connection:
             connection.execute(
-                sqlalchemy.text(f"INSERT INTO users (login, password, description) VALUES ('{new_user.login}', '{new_user.password}', '{new_user.description}')"))
+                sqlalchemy.text(f"INSERT INTO users (login, password, description, list_posts) VALUES ('{new_user.login}', '{new_user.password}', '{new_user.description}', '')"))
             connection.commit()
-
-        return 'Вы успешно зарегистрированы!'
+            session['username'] = new_user.login
+            session['password'] = new_user.password
+            session['description'] = new_user.description
+        return redirect(url_for('main'), 301)
 
     return render_template('register.html')
 
@@ -42,15 +90,31 @@ def login():
     if request.method == 'POST':
         login = request.form['username']
         password = request.form['password']
-
-        # Проверка наличия пользователя в базе данных
         df = pd.read_sql_table('users', engine)
         if login in df['login'].values and password == df.loc[df['login'] == login, 'password'].values[0]:
-            return 'Вы успешно вошли!'
+            session['username'] = login
+            session['password'] = password
+            return redirect(url_for('main'), 301)
         else:
             return 'Неверный логин или пароль.'
 
     return render_template('login.html')
+
+
+@app.route('/create_post', methods=['GET', 'POST'])
+def create_post():
+    if request.method == 'POST':
+        new_post = Post(request.form['title'], request.form['class'], session['username'], request.form['text'])
+        df = pd.read_sql_table('posts', engine)
+        if new_post.title in df['title'].values:
+            return 'Запись с таким названием уже существует'
+        with engine.connect() as connection:
+            connection.execute(
+                sqlalchemy.text(f"INSERT INTO posts_proverka (post_id,title, user_name, type, text) VALUES (NUll, '{new_post.title}', '{new_post.user_name}', '{new_post.type}', '{new_post.text}')"))
+            connection.commit()
+        return 'Запись создана'
+
+    return render_template('post.html')
 
 
 if __name__ == '__main__':
