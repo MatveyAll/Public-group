10d1eced42edd3043933645649f79b918988029c
las-tochka <takuve@yandex.ru>
2024-04-14 22:26:36 +0300

    styles + answer

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import base64
+
 from flask import Flask, url_for, redirect, render_template, request, session
 import pandas as pd
 import sqlalchemy
-from selenium.webdriver.common.keys import Keys
 
 # подключение к базе
 engine = sqlalchemy.create_engine('sqlite:///flask-database.db')
@@ -17,16 +18,20 @@ class User:
 
 
 class Post:
-    def __init__(self, title, type, user_name, text):
+    def __init__(self, title, type, user_name, text, audio, photo):
         self.title = title
         self.type = type
         self.user_name = user_name
         self.text = text
+        self.audio = audio
+        self.photo = photo
 
 
 @app.route('/')
 @app.route('/main', methods=['GET', 'POST'])
 def main():
+    if not (session['username'] and session['password']):  # проверка был ли вход у пользователя
+        return redirect(url_for('login'), 301)
     df = pd.read_sql_table('posts', engine)
     posts = df.to_dict('records')
     type = ''
@@ -40,7 +45,7 @@ def main():
 @app.route('/moderation', methods=['GET', 'POST'])
 def moderation():
     if session['username'] != 'admin' and session['password'] != '54321':
-        return 'Вы не являетесь админом и не можете модерировать записи'
+        return redirect('/answer/Вы не являетесь админом и не можете модерировать записи/moderation')
     df = pd.read_sql_table('posts_proverka', engine)
     posts = df.to_dict('records')
 
@@ -53,17 +58,26 @@ def moderation():
             post_id = request.form['post_id']
 
             with engine.connect() as connection:
-                connection.execute(
-                    sqlalchemy.text(
-                        "INSERT INTO posts (post_id, title, user_name, type, text) VALUES (:post_id, :title, :user_name, :type, :text)"),
-                    {"post_id": post_id, "title": title, "user_name": user_name, "type": typed, "text": text}
-                )
+                if len(request.form) == 7:
+                    connection.execute(
+                        sqlalchemy.text(
+                            "INSERT INTO posts (post_id, title, user_name, type, text, audio, photo) VALUES (:post_id, :title, :user_name, :type, :text, :audio, :photo)"),
+                        {"post_id": post_id, "title": title, "user_name": user_name, "type": typed, "text": text,
+                         "audio": request.form['audio'], "photo": request.form['photo']}
+                    )
+                else:
+                    connection.execute(
+                        sqlalchemy.text(
+                            "INSERT INTO posts (post_id, title, user_name, type, text, audio, photo) VALUES (:post_id, :title, :user_name, :type, :text, :audio, :photo)"),
+                        {"post_id": post_id, "title": title, "user_name": user_name, "type": typed, "text": text,
+                         "audio": "", "photo": ""}
+                    )
                 connection.execute(
                     sqlalchemy.text("DELETE FROM posts_proverka WHERE post_id = :post_id"),
                     {"post_id": post_id}
                 )
                 connection.commit()
-                #Обновление страницы чтобы одобренная заметка исчезла
+                return redirect(url_for('moderation'), 301)
 
     return render_template('moderation.html', posts=posts)
 
@@ -76,7 +90,7 @@ def register():
         df = pd.read_sql_table('users', engine)
 
         if new_user.login in df['login'].values:
-            return 'Пользователь с таким логином уже существует!'
+            return redirect('/answer/Пользователь с таким логином уже существует!/register')
         with engine.connect() as connection:
             connection.execute(
                 sqlalchemy.text(f"INSERT INTO users (login, password, description, list_posts) VALUES ('{new_user.login}', '{new_user.password}', '{new_user.description}', '')"))
@@ -87,6 +101,11 @@ def register():
         return redirect(url_for('main'), 301)
 
     return render_template('register.html')
+
+
+@app.route('/answer/<text>/<fromm>', methods=['GET', 'POST'])
+def answer(text, fromm):
+    return render_template('answer.html', text=text, fromm='/' + fromm)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -100,7 +119,7 @@ def login():
             session['password'] = password
             return redirect(url_for('main'), 301)
         else:
-            return 'Неверный логин или пароль.'
+            return redirect('answer/Неверный логин или пароль./login')
 
     return render_template('login.html')
 
@@ -108,15 +127,25 @@ def login():
 @app.route('/create_post', methods=['GET', 'POST'])
 def create_post():
     if request.method == 'POST':
-        new_post = Post(request.form['title'], request.form['class'], session['username'], request.form['text'])
+        new_post = Post(
+            request.form['title'],
+            request.form['class'],
+            session['username'],
+            request.form['text'],
+            base64.b64encode(request.files['audio'].read()).decode('utf-8'),
+            base64.b64encode(request.files['photo'].read()).decode('utf-8')
+        )
         df = pd.read_sql_table('posts', engine)
         if new_post.title in df['title'].values:
-            return 'Запись с таким названием уже существует'
+            return redirect('/answer/Запись с таким названием уже существует/create_post')
         with engine.connect() as connection:
             connection.execute(
-                sqlalchemy.text(f"INSERT INTO posts_proverka (post_id,title, user_name, type, text) VALUES (NUll, '{new_post.title}', '{new_post.user_name}', '{new_post.type}', '{new_post.text}')"))
+                sqlalchemy.text(
+                    f"INSERT INTO posts_proverka (post_id,title, user_name, type, text, audio, photo) VALUES (NUll, '{new_post.title}', '{new_post.user_name}', '{new_post.type}', '{new_post.text}', :audio, :photo)"),
+                {"audio": new_post.audio, "photo": new_post.photo}
+            )
             connection.commit()
-        return 'Запись создана'
+        return redirect('answer/Запись создана/create_post')
 
     return render_template('post.html')
 
